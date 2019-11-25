2f059bdd61f8ce0c7b172eba9fc25671bcca746d
AlexCharnamordzin <55506894+AlexCharnamordzin@users.noreply.github.com>
2019-11-25 18:13:13 +0300

    beforeowl

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,26 @@
-console.log('Привет')
+$(function () {
+    var fixblock_pos = $('#fixblock').position().top;
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > fixblock_pos) {
+            $('#fixblock').css({
+                'position': 'fixed',
+                'top': '0px',
+                'background': 'white',
+                'width': '100%',
+                'opacity':'.8',
+            });
+        } else {
+            $('#fixblock').css({
+                'position': 'static',
+                'background': 'none'
+            });
+
+        }
+    })
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+  
